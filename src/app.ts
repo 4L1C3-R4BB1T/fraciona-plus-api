b2e02cbeae92bf74bc58270a7183b56c5c774df2
Livia <livinha.guimaraes.013@gmail.com>
2024-11-01 14:55:34 -0300

    feat: config firebase

--- src/app.ts
+++ src/app.ts
@@ -22,8 +22,14 @@ mongoose.connect(process.env.MONGODB_URL)
 
 async function bootstrap() {
     // Inicializando o Firebase 
+    // firebaseAdmin.initializeApp({
+    //     credential: firebaseAdmin.credential.cert(process.env.FIREBASE_SECRET_KEY!),
+    // });
+    
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SECRET_KEY!);
+
     firebaseAdmin.initializeApp({
-        credential: firebaseAdmin.credential.cert(process.env.FIREBASE_SECRET_KEY!),
+        credential: firebaseAdmin.credential.cert(serviceAccount),
     });
 
     const port = process.env.PORT ?? '3000';
